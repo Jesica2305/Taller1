959c634d5db406abe2793b3d892c1e6a373737f7
miguel006 <brnjamincarmine@gmail.com>
2023-08-18 16:06:28 -0500

    Divisiòn.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("Ingrese el dividendo (número a dividir): ");
+            int dividendo = sc.nextInt();
+
+            System.out.print("Ingrese el divisor (número por el cual dividir): ");
+            int divisor = sc.nextInt();
+
+            double result = (double) dividendo / divisor;
+
+            boolean divisionExacta = dividendo % divisor == 0;
+
+            System.out.println("Resultado de la división: " + result);
+            if (divisionExacta) {
+                System.out.println("La división es exacta.");
+            } else {
+                System.out.println("La división no es exacta.");
+            }
+
+        }
     }
-}
+
+
